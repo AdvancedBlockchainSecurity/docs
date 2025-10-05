08e76fffc58a62a5a2a4b9550a1819e272844d52
Dehvon C <dehvonc@gmail.com>
2025-10-04 20:37:11 -0600

    updated files

---
+++ Sprints/Sprint-1/commit.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+cd solidity-security-analysis
+gitmain
+
+cd ../solidity-security-api-service
+gitmain
+
+cd ../solidity-security-aws-infrastructure
+gitmain
+
+cd ../solidity-security-contract-parser
+gitmain
+
+cd ../solidity-security-dashboard
+gitmain
+
+cd ../solidity-security-data-service
+gitmain
+
+cd ../solidity-security-docs
+gitmain
+
+cd ../solidity-security-findings
+gitmain
+
+cd ../solidity-security-intelligence-engine
+gitmain
+
+cd ../solidity-security-monitoring
+gitmain
+
+cd ../solidity-security-notification
+gitmain
+
+cd ../solidity-security-orchestration
+gitmain
+
+cd ../solidity-security-shared
+gitmain
+
+cd ../solidity-security-tool-integration
+gitmain
+
+cd ../solidity-security-tools
+gitmain
+
+cd ../solidity-security-ui-core
+gitmain
+
+cd ../solidity-security-vulnerabilities
+gitmain
